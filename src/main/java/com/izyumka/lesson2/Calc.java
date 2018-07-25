a932abf1621061d073ae3731a3cf14306dac4802
izyumka <izyumka21@gmail.com>
2018-07-25 21:22:48 +0300

    add not default construction

--- src/main/java/com/izyumka/lesson2/Calc.java
+++ src/main/java/com/izyumka/lesson2/Calc.java
@@ -1,16 +1,30 @@
 package com.izyumka.lesson2;
 
 public  class Calc {
-    public static int add(int a, int b) {
+    public int chargelevel;
+    public int accurancy;
+
+    public Calc() {
+    }
+
+    public Calc(int chargelevel, int accurancy) {
+        this.chargelevel = chargelevel;
+        this.accurancy = accurancy;
+    }
+
+    public static int add(int a, int b)
+    {
         return a + b;
     }
 
    public static int odd(int a, int b) {
         return a - b;
     }
+
     public static int mult(int a, int b) {
         return a*b;
     }
+
     public static double div (double a, double b) {
         return a/b;
     }
--- src/main/java/com/izyumka/lesson2/Main.java
+++ src/main/java/com/izyumka/lesson2/Main.java
@@ -1,13 +1,20 @@
 package com.izyumka.lesson2;
 
 public class Main {
+    public static Calc calc2;
     public static void main(String[] args) {
         Calc calc = new Calc();
+        calc2 = new Calc();
+        Calc calc3 = new Calc(80, 100);
         System.out.println(Calc.add(3, 2));
         System.out.println(calc.odd(4,3));
         System.out.println(calc.mult(3,4));
         System.out.println(calc.div(4, 2));
         System.out.println((calc.sqrt(16)));
         System.out.println(calc.sqrt(4));
+        System.out.println(calc);
+        System.out.println(calc2);
+        System.out.println(calc3.accurancy);
+        System.out.println(calc3.chargelevel);
     }
 }
